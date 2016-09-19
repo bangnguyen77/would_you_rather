d284dee8ed05e737b8403a62f70d8d2bf671f036
Bang Nguyen and Matthew Giem <bang+matthew@>
2016-09-19 10:35:15 -0700

    create CRUD ability to questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -21,12 +21,22 @@ class QuestionsController < ApplicationController
   end
 
   def edit
+    @question = Question.find(params[:id])
   end
 
   def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to questions_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
   end
 
   private
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ActiveRecord::Base
   validates_presence_of :statement
 
   has_many :answers
+  belongs_to :user
 end
