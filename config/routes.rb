f7a17cab25a59fc49e24ddbdb7a6800fede925df
Bang Nguyen and matthew giem <bang+matthew@>
2016-09-20 10:26:11 -0700

    clean up the home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :questions
 
-  root "pages#show", page: "home"
-  post 'post/:id' => 'questions#updateAnswer1'
+  root "questions#index"
 end
