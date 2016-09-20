95fb7c8174925288c9dcf43905eae441f5f92dc9
Bang Nguyen and matthew giem <bang+matthew@>
2016-09-20 16:10:01 -0700

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+would_you_rather = [
+  ["question1","option1a","option1b"],
+  ["question2","option2a","option2b"],
+  ["question3","option3a","option3b"],
+  ["question4","option4a","option4b"],
+  ["question5","option5a","option5b"],
+  ["question6","option6a","option6b"],
+  ["question7","option7a","option7b"],
+  ["question8","option8a","option8b"]
+]
+
+
+would_you_rather.each do |question, answer1, answer2|
+  Question.create( statement: question, answer1: answer1, answer2: answer2)
+end
